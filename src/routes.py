2db3a0c01c22a83bb17cd619210ddbdebf2b3a55
DinukaNavaratna <dinuka.navaratna@gmail.com>
2023-10-22 02:59:12 +0000

    Teacher view

--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,5 @@
 from .principle import dashboard, feedback, academic, k6, manbox, masculinity, engagement, growthmindset
-from .teacher import Tdashboard, Tfeedback, Tk6, Tdisrespect
+from .teacher import Tdashboard, Tfeedback, Tk6, Tdisrespect, Tmanbox, Tmasculinity, Tengagement, Tgrowthmindset, Tstudent
 
 def initialize_routes(api):
     api.add_resource(dashboard, "/dashboard")
@@ -14,3 +14,8 @@ def initialize_routes(api):
     api.add_resource(Tfeedback, "/Tfeedback")
     api.add_resource(Tk6, "/Tk6")
     api.add_resource(Tdisrespect, "/Tdisrespect")
+    api.add_resource(Tmanbox, "/Tmanbox")
+    api.add_resource(Tmasculinity, "/Tmasculinity")
+    api.add_resource(Tengagement, "/Tengagement")
+    api.add_resource(Tgrowthmindset, "/Tgrowthmindset")
+    api.add_resource(Tstudent, "/Tstudent")
--- src/teacher.py
+++ src/teacher.py
@@ -5,6 +5,7 @@ from pandas import read_excel
 from loguru import logger
 from faker import Faker
 import json
+import numpy as np
 
 
 # data source
@@ -117,11 +118,11 @@ class Tk6(Resource):
             
             result = {}
             
-            abnormal = df[['First-Name', 'Last-Name', 'Email', 'CompleteYears', 'House', 'k6_overall']][(df['k6_overall'] >= 20)].sort_values(by='k6_overall')
+            abnormal = df[['First-Name', 'Last-Name', 'Email', 'CompleteYears', 'House', 'k6_overall', 'Participant-ID']][(df['k6_overall'] >= 20)].sort_values(by='k6_overall')
             abnormal = abnormal.values.tolist()
             result['abnormal'] = abnormal
             
-            borderline = df[['First-Name', 'Last-Name', 'Email', 'CompleteYears', 'House', 'k6_overall']][(df['k6_overall'] >= 16) & (df['k6_overall'] < 20)].sort_values(by='k6_overall')
+            borderline = df[['First-Name', 'Last-Name', 'Email', 'CompleteYears', 'House', 'k6_overall', 'Participant-ID']][(df['k6_overall'] >= 16) & (df['k6_overall'] < 20)].sort_values(by='k6_overall')
             borderline = borderline.values.tolist()
             result['borderline'] = borderline
 
@@ -149,9 +150,12 @@ class Tdisrespect(Resource):
             disrespectful = read_excel(xlsx, sheet_name= 'net_5_Disrespect')
             disrespectful = disrespectful.rename(columns={'Source': 'Participant-ID', 'Target': 'Target'})
             disrespectful_participant = read_excel(xlsx, sheet_name='participants')
+            disrespectful_participant1 = read_excel(xlsx, sheet_name='participants')
+            disrespectful_participant1 = disrespectful_participant1.rename(columns={'Participant-ID': 'Target', 'First-Name': 'FirstName', 'Last-Name': 'LastName', 'House': 'Ignore'})
             disrespectful_plot = pd.merge(disrespectful, disrespectful_participant, on=['Participant-ID'])
-            disrespectful_plot = disrespectful_plot[['Participant-ID', 'First-Name', 'Last-Name', 'Target']][disrespectful_plot['House'] == house]
-            result['disrespectee'] = disrespectful_plot.values.tolist()
+            disrespectful_plot = pd.merge(disrespectful_plot, disrespectful_participant1, on=['Target'])
+            disrespectful_plot = disrespectful_plot[['Participant-ID', 'First-Name', 'Last-Name', 'Target', 'FirstName', 'LastName']][disrespectful_plot['House'] == house]
+            result['disrespect'] = disrespectful_plot.values.tolist()
             
             resultstring = json.dumps(result)
             if("NaN" in resultstring):
@@ -161,6 +165,138 @@ class Tdisrespect(Resource):
             return result
         except Exception as ex:
             return "failed - "+str(ex)
+
+
+class Tmanbox(Resource):
+    def post(self):
+        try:
+            request_body = request.json
+            survey = request_body["survey"]
+            house = request_body["house"]
+            
+            df, xlsx = dataCleaning(survey, house)
+            
+            result = {}
+            
+            threshold = df['Manbox5_overall'].quantile(0.95)
+            manbox = df[['Participant-ID', 'First-Name', 'Last-Name', 'CompleteYears', 'Manbox5_overall']][df['Manbox5_overall'] >= threshold]
+            manbox = manbox.values.tolist()
+            result['manbox'] = manbox
+
+            resultstring = json.dumps(result)
+            if("NaN" in resultstring):
+                resultstring = resultstring.replace("NaN", "0")
+                result = json.loads(resultstring)
+        
+            return result
+        except Exception as ex:
+            return "failed - "+str(ex)
+
+
+class Tmasculinity(Resource):
+    def post(self):
+        try:
+            request_body = request.json
+            survey = request_body["survey"]
+            house = request_body["house"]
+            
+            df, xlsx = dataCleaning(survey, house)
+            
+            result = {}
+            
+            threshold = df['Masculinity_contrained'].quantile(0.95)
+            masculinity = df[['Participant-ID', 'First-Name', 'Last-Name', 'CompleteYears', 'Masculinity_contrained']][df['Masculinity_contrained'] >= threshold]
+            masculinity = masculinity.values.tolist()
+            result['masculinity'] = masculinity
+
+            resultstring = json.dumps(result)
+            if("NaN" in resultstring):
+                resultstring = resultstring.replace("NaN", "0")
+                result = json.loads(resultstring)
+        
+            return result
+        except Exception as ex:
+            return "failed - "+str(ex)
+
+
+class Tengagement(Resource):
+    def post(self):
+        try:
+            request_body = request.json
+            survey = request_body["survey"]
+            house = request_body["house"]
+            
+            df, xlsx = dataCleaning(survey, house)
+            
+            result = {}
+            
+            threshold = df['School_support_engage6'].quantile(0.95)
+            engagement = df[['Participant-ID', 'First-Name', 'Last-Name', 'CompleteYears', 'School_support_engage6']][df['School_support_engage6'] >= threshold]
+            engagement = engagement.values.tolist()
+            result['engagement'] = engagement
+
+            resultstring = json.dumps(result)
+            if("NaN" in resultstring):
+                resultstring = resultstring.replace("NaN", "0")
+                result = json.loads(resultstring)
+        
+            return result
+        except Exception as ex:
+            return "failed - "+str(ex)
+
+
+class Tgrowthmindset(Resource):
+    def post(self):
+        try:
+            request_body = request.json
+            survey = request_body["survey"]
+            house = request_body["house"]
+            
+            df, xlsx = dataCleaning(survey, house)
+            
+            result = {}
+            
+            threshold = df['GrowthMindset'].quantile(0.95)
+            growthmindset = df[['Participant-ID', 'First-Name', 'Last-Name', 'CompleteYears', 'GrowthMindset']][df['GrowthMindset'] >= threshold]
+            growthmindset = growthmindset.values.tolist()
+            result['growthmindset'] = growthmindset
+
+            resultstring = json.dumps(result)
+            if("NaN" in resultstring):
+                resultstring = resultstring.replace("NaN", "0")
+                result = json.loads(resultstring)
+        
+            return result
+        except Exception as ex:
+            return "failed - "+str(ex)
+
+
+class Tstudent(Resource):
+    def post(self):
+        try:
+            request_body = request.json
+            survey = request_body["survey"]
+            category = request_body["category"]
+            sid = np.int64(request_body["sid"])
+            
+            df, xlsx = dataCleaning(survey, "")
+            
+            result = {}
+            
+            sna_student = read_excel(xlsx, sheet_name=str(category))
+            sna_student.drop(sna_student[sna_student['Source'] != sid].index, inplace=True)
+        
+            result['network'] = (sna_student.values).tolist()
+
+            resultstring = json.dumps(result)
+            if("NaN" in resultstring):
+                resultstring = resultstring.replace("NaN", "0")
+                result = json.loads(resultstring)
+        
+            return result
+        except Exception as ex:
+            return "failed - "+str(ex)
+
 
 
 
