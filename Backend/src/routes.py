09aa71f33a9c29a70257de5fe61d304bf619dffd
Dinuka Navaratna <Dinuka.navaratna@gmail.com>
2023-10-23 17:06:32 +1100

    Added adjanency for teachers

--- Backend/src/routes.py
+++ Backend/src/routes.py
@@ -1,6 +1,6 @@
 from .principle import dashboard, feedback, academic, k6, manbox, masculinity, engagement, growthmindset, clubs
 from .outsider import Odashboard, Ofeedback, Oacademic, Ok6, Omanbox, Omasculinity, Oengagement, Ogrowthmindset, Oclubs
-from .teacher import Tdashboard, Tfeedback, Tk6, Tdisrespect, Tmanbox, Tmasculinity, Tengagement, Tgrowthmindset, Tstudent, Tdata
+from .teacher import Tdashboard, Tfeedback, Tk6, Tdisrespect, Tmanbox, Tmasculinity, Tengagement, Tgrowthmindset, Tstudent, Tdata, Tadjanency
 from .student import Sdashboard, Snominations, Scolleague
 
 def initialize_routes(api):
@@ -24,6 +24,7 @@ def initialize_routes(api):
     api.add_resource(Tgrowthmindset, "/Tgrowthmindset")
     api.add_resource(Tstudent, "/Tstudent")
     api.add_resource(Tdata, "/Tdata")
+    api.add_resource(Tadjanency, "/Tadjanency")
     
     api.add_resource(Sdashboard, "/Sdashboard")
     api.add_resource(Snominations, "/Snominations")
